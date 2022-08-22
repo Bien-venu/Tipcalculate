0c5a9ef0fb3bc810f2c7b695575e30e7de968f05
2Bienvenu <ishimwesibomanabienvenu@gmail.com>
2022-08-22 18:05:41 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const billInput = document.getElementById("inbill");
+const tipInput = document.getElementById("tipInput");
+const numberOfPeopleViv = document.getElementById("numberOfPeople");
+const perPersonTotal = document.getElementById("perPersonTotal");
+
+// let numberOfPeople = Number(numberOfPeopleViv.value);
+
+const calculateBill = () => {
+  const bill = billInput.value;
+  console.log(bill);
+};
+const increasePeople = () => {};
+const decreasePeople = () => {};
